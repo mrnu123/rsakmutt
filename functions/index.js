a72310e4f95c93bad90c9960d0883a8c04d0567b
mrnu123 <49053059+mrnu123@users.noreply.github.com>
2019-11-18 02:26:58 +0700

    dep fn

--- functions/index.js
+++ functions/index.js
@@ -23,9 +23,9 @@ exports.app = functions.https.onRequest(app);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+ exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+ });
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
