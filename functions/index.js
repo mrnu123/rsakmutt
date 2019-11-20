e5f0335b91efd4ed56e0852d2a97bda0ff365d36
mrnu123 <49053059+mrnu123@users.noreply.github.com>
2019-11-21 01:13:45 +0700

    redir fn to host

--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,40 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 // The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
-admin.initializeApp();
+//const admin = require('firebase-admin');
+//admin.initializeApp();
 
 const express = require('express');
 const app = express();
-const port = 5001;
+//const port = 5001;
 
-app.listen(port, function(){
-    console.log("Listening to port"+port);
-});
-
-
-app.get('/ping',(req,res)=>{
-    res.send("Of course I still love you");
-});
+// app.listen(port, function(){
+//     console.log("Listening to port"+port);
+// });
 
 exports.app = functions.https.onRequest(app);
+app.get('/home',(req,res)=>{
+    res.send("Success!!");
+});
+
+
 
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
- exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
+//  exports.helloWorld = functions.https.onRequest((request, response) => {
+//   response.send("Hello from Firebase!");
+//  });
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
+//exports.addMessage = functions.https.onRequest(async (req, res) => {
     // Grab the text parameter.
-    const original = req.query.text;
+   // const original = req.query.text;
     // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    const snapshot = await admin.database().ref('/messages').push({original: original});
+  //  const snapshot = await admin.database().ref('/messages').push({original: original});
     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    res.redirect(303, snapshot.ref.toString());
-  });
+  //  res.redirect(303, snapshot.ref.toString());
+ // });
 
